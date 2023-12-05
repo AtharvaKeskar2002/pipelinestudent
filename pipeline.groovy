f5ad81be9193bc3a0dab167ad20ecd10fd80f474
AtharvaKeskar2002 <keskaratharva27@gmail.com>
2023-12-05 17:09:30 +0530

    atharva

--- pipeline.groovy
+++ pipeline.groovy
@@ -38,6 +38,7 @@ pipeline {
                     sh'''
                         cd ..
                         mv /var/lib/jenkins/workspace/pipeline1/target/student.war /var/lib/jenkins/workspace/pipeline1/docker/studetnapp
+                        pwd
                         cd docker
                         cd proxy
                         sudo docker build -t frontendcicdstudent .
